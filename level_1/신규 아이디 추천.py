a42753fe5586719ffd0e79b3efa75d21edaa5bd0
Bean <whddkrak@gmail.com>
2021-09-02 22:56:05 +0900

    [FIX] 신규 아이디 추천

--- level_1/신규 아이디 추천.py
+++ level_1/신규 아이디 추천.py
@@ -4,19 +4,23 @@ def solution(new_id):
     
     #1단계
     new_id = new_id.lower()
+    
     #2단계
     for i in range(len(char)):
         new_id = new_id.replace(char[i], "")
     
     #3단계
-    for j in range(len(new_id)):
+    while '..' in new_id: 
         new_id = new_id.replace('..', '.')
     
     #4단계
-    
     if new_id[0] == '.':
-        new_id = new_id[1:]
-    elif new_id[-1] == '.':
+        if len(new_id) >=2:  #index error exception
+            new_id = new_id[1:]
+        else:
+            new_id = '.'
+            
+    if new_id[-1] == '.':
         new_id = new_id[:-1]
     
     #5단계
@@ -30,7 +34,7 @@ def solution(new_id):
             new_id = new_id[:-1]
     
     #7단계
-    while(len(new_id) < 3):
+    while len(new_id) < 3:
         new_id = new_id + new_id[-1]
             
     answer = new_id
