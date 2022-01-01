214966cf9b18f60b56cfab9a670ad7c7ceb9840d
Bean <whddkrak@gmail.com>
2022-01-01 22:43:57 +0900

    [level1] K번째수

---
+++ level_1/K번째수.js
@@ -0,0 +1,12 @@
+function solution(array, commands) {
+  var answer = [];
+  
+  for (let i = 0; i < commands.length; i++) {
+      let newArr = array.slice(commands[i][0] -1, commands[i][1]);
+      newArr.sort(function(a,b) {
+          return a - b;
+      });
+      answer.push(newArr[commands[i][2] - 1]);
+  }    
+  return answer;
+}
