5d06c2b1655a334ba2f5e01c967d3807b5036c1b
Bean <whddkrak@gmail.com>
2022-01-12 23:34:05 +0900

    [level2] H-index

--- level_2/H-Index.js
+++ level_2/H-Index.js
@@ -1,4 +1,16 @@
 function solution(citations) {
   var answer = 0;
+  //오름차순 정렬
+  citations = citations.sort((a, b) => a - b)
+  let n = citations.length
+  
+  for (let i = 0; i < n; i++) {
+      let Hindex = n-i
+      if (Hindex <= citations[i]) {
+          answer = Hindex
+          break
+      }
+  }
+  
   return answer;
 }
