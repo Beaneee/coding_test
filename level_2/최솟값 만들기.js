116bc212f7241698369213d7e47ace26fd1cf79d
종빈 <whddkrak@gmail.com>
2023-09-01 11:48:11 +0900

    refactor: [level2] 최솟값 만들기

--- level_2/최솟값 만들기.js
+++ level_2/최솟값 만들기.js
@@ -1,11 +1,20 @@
-function solution(A,B){
-  let answer = 0;
-  let arrA = A.sort((a, b) => b - a);
-  let arrB = B.sort((a, b) => a - b);
+// 1.
+// function solution(A,B){
+//   let answer = 0;
+//   let arrA = A.sort((a, b) => b - a);
+//   let arrB = B.sort((a, b) => a - b);
   
-  for(let i = 0; i < A.length; i++) {
-      let result = arrA[i] * arrB[i];
-      answer += result;
-  } 
-  return answer;
+//   for(let i = 0; i < A.length; i++) {
+//       let result = arrA[i] * arrB[i];
+//       answer += result;
+//   } 
+//   return answer;
+// }
+
+// 2. 리팩토링 -> use reduce
+function solution(A,B){
+  A.sort((a, b) => b - a);
+  B.sort((a, b) => a - b);
+  
+  return A.reduce((acc, curr, idx) => acc + curr * B[idx], 0);
 }
