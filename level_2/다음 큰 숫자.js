4fe11747bf2799fbbd2f4f1594f75f64fd753b48
종빈 <whddkrak@gmail.com>
2023-09-04 11:04:38 +0900

    refactor: [level2] 다음 큰 숫자

--- level_2/다음 큰 숫자.js
+++ level_2/다음 큰 숫자.js
@@ -1,20 +1,18 @@
-function solution(n) {
-  let answer = 0;
-  //n을 이진수로 변환하고
-  let binaryNum = n.toString(2);
-  //그걸 배열로 반환한다. -> 1의 갯수를 세기위해서
-  let splitStr = binaryNum.split('')
-  //1의 갯수
-  let count = splitStr.filter((v) => v === '1').length;
+function solution(n) { 
+  let count = countOfOne(n);
   let result = n;
   while(true) {
-      result++;
-      let resultBinaryNum = result.toString(2);
-      let resultSplitStr = resultBinaryNum.split('');
-      let resultCount = resultSplitStr.filter((v) => v === '1').length;
-      
+      result++;    
+      let resultCount = countOfOne(result);
       if(resultCount === count) break;
   }
   
   return result;
+}
+
+function countOfOne(num) {
+  let binaryNum = num.toString(2);
+  let splitStr = binaryNum.split('');
+  let count = splitStr.filter((v) => v === '1').length;
+  return count;
 }
