74e25d0b6a1126c21a50d7309564941d37b9581b
종빈 <whddkrak@gmail.com>
2023-09-05 22:31:43 +0900

    [level2] 피보나치 수

--- level_2/피보나치 수.js
+++ level_2/피보나치 수.js
@@ -1,11 +1,13 @@
+// BigInt 는 Number 원시 값이 안정적으로 나타낼 수 있는 최대치인 2^53 - 1보다 큰 정수를 표현할 수 있는 내장 객체입니다.
+
 function solution(n) {
   //피보나치수열을 배열로 만들어볼까?
-  let arr = [0, 1];
+  let arr = [0n, 1n];
   
   for(let i = 2; i <= n; i++) {
-      arr.push(arr[i-2] + arr[i-1]); //배열로 해서 실패하는건가? -> infinity
+      arr.push(BigInt(arr[i-2]) + BigInt(arr[i-1])); //배열로 해서 실패하는건가? -> infinity
   }
-  let result = arr[n] % 1234567
+  let result = arr[n] % 1234567n
   
   return result;
 }
