7039409806d14caa900eec391b515c70b9580056
종빈 <whddkrak@gmail.com>
2024-02-27 23:51:18 +0900

    [level3] 정수 삼각형 : 홀수와 짝수 index일 경우 생각해보기

---
+++ level_3/정수 삼각형.js
@@ -0,0 +1,16 @@
+function solution(triangle) {
+  var answer = 0;
+  let length = triangle.length;
+  
+  if(length === 1) {
+      return triangle[0]
+  }
+  
+  if(length % 2 === 0) {
+      
+  } else {
+      
+  }
+  
+  return answer;
+}
