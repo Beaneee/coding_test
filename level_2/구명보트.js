45ba9cb4de472102623356786b7850868dc6e910
종빈 <whddkrak@gmail.com>
2024-03-05 23:57:51 +0900

    [level2] 구명보트 - 틀린 이유 분석

--- level_2/구명보트.js
+++ level_2/구명보트.js
@@ -1,29 +1,34 @@
 function solution(people, limit) {
-    
     let count = 0;
     const initRecursiveCount = people.length;
-    let copyArr = people.sort((a, b) => b - a);
-    
-    
-    for(i = 0; i < initRecursiveCount; i++) {
-        const weight = copyArr[0]; 
+    /**
+     * 배열의 복사와 정렬로 인한 추가 비용 발생 -> sort는 필요해보인다.
+     */
+    let copyArr = people.slice().sort((a, b) => b - a);
+
+    /**
+     * 남은 사람 중 가장 무거운 사람과 가장 가벼운 사람을 함께 탑승시키는 방법을 고려하지 않고 있다.
+     */
+    for (let i = 0; i < initRecursiveCount; i++) {
+        const weight = copyArr[0];
         const differ = limit - weight;
-        
-        if(weight > differ) {
+
+        if (weight > differ) {
             count++;
-            people.shift();
+            /**
+             * 매 반복마다 배열의 길이를 줄이는 작업이기 때문에 배열의 길이만큼의 반복이 필요하다. 
+             * 따라서 이 코드는 최악의 경우 O(n^2)의 시간복잡도를 가지게 된다.
+             */
+            copyArr.shift();
         } else {
-            
-            if(copyArr.length > 1) {
-                count += (copyArr.length / 2)
+            if (copyArr.length > 1) {
+                count += Math.ceil(copyArr.length / 2);
+            } else {
+                count++;
             }
-            
-            if(copyArr.length === 1) {
-                count++
-            }
-            copyArr = [];
+            break;
         }
     }
-    
+
     return count;
 }
