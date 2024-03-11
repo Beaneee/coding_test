f9aa85fb82fd2e3bb09c30fcb0f6ce4fb89cd5d2
종빈 <whddkrak@gmail.com>
2024-03-12 00:10:53 +0900

    [level2] 예상 대진표 #2

--- level_2/예상 대진표.js
+++ level_2/예상 대진표.js
@@ -6,16 +6,23 @@ function solution(n,a,b) {
   let count = 1;
   
   while(total !== 1) {
-      
-      if(max - min === 1) {
-          return count
-      }
-      
-      total = total / 2;
-      count++;
-      
-      max % 2 === 0 ? max = max / 2 : max = (max + 1) / 2;
-      min % 2 === 0 ? min = min / 2 : min = (min + 1) / 2;
-      solution(total, max, min);
+    max % 2 === 0 ? max = max / 2 : max = (max + 1) / 2;
+    min % 2 === 0 ? min = min / 2 : min = (min + 1) / 2;
+
+    if(Math.ceil(max) === Math.ceil(min)) {
+      return count;
+    }
+
+    total = total / 2;
+    count++;
   }
+}
+
+//틀린 이유
+//종료 조건이 잘못되었다.
+//예를들어, 2-3일 경우에도 max - min = 1이다.
+
+//종료조건은 max === min이다.
+if(max - min === 1) {
+  return count
 }
