10044e1193a0d3a0e893106104857d00fb0e7d20
종빈 <whddkrak@gmail.com>
2024-03-12 23:28:33 +0900

    [level2] 멀리뛰기 #1

---
+++ level_2/멀리뛰기.js
@@ -0,0 +1,11 @@
+//피보나치 수열
+function solution(n) {
+  //f1, f2
+  let arr = [1, 2];
+  
+  for(i = 2; i < n; i++) {
+      arr.push(arr[i - 1] + arr[i - 2])
+  }
+  
+  return arr[arr.length - 1]
+}
