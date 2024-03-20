8647a6d534db658032f81e51eb37dd40eaeb8719
종빈 <whddkrak@gmail.com>
2024-03-20 23:23:34 +0900

    [level1] 약수의 합 #2

--- level_1/약수의 합.js
+++ level_1/약수의 합.js
@@ -1,17 +1,11 @@
 function solution(n) {
   let arr = []
   
-  for(let i = 0; i <= n; i++) {
+  for(let i = 1; i <= n; i++) {
       if(n % i === 0) {
           arr.push(i)
       }
   }
   
-  let sum = 0;
-  
-  for(let j = 0; j < arr.length; j++) {
-      sum += arr[j]
-  }
-  
-  return sum
+  return arr.reduce((acc, curr) => acc + curr, 0)
 }
