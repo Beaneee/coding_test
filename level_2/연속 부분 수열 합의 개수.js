e3386bafa7316963e23fbeac0fcfe8e97e81bd36
종빈 <whddkrak@gmail.com>
2024-03-26 01:41:52 +0900

    [level2] 연속 부분 수열 합의 개수 #1

---
+++ level_2/연속 부분 수열 합의 개수.js
@@ -0,0 +1,13 @@
+function solution(elements) {
+  const result = [];
+  
+  for(let length = 1; length <= elements.length; length++) {
+      for(let i = 0; i < elements.length - length; i++) {
+          const subarray = elements.slice(i, i + length);
+          const sum = subarray.reduce((acc, curr) => acc + curr, 0); // 부분 배열의 요소들의 합 계산
+          result.push(sum);
+      }
+  }
+  
+  console.log(result);
+}
