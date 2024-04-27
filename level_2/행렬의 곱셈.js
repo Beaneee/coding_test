d5908c445c98c2d88b741c26e1ca35429c32fdaa
종빈 <whddkrak@gmail.com>
2024-04-27 16:33:15 +0900

    [level2] 행렬의 곱셈 #1

---
+++ level_2/행렬의 곱셈.js
@@ -0,0 +1,26 @@
+function solution(arr1, arr2) {
+  //arr2를 변환 시키자.
+  let arr2Column = [];
+  let result = [];
+  for(let i = 0; i < arr2.length; i++) {
+    let temp = [];
+    for(let j = 0; j < arr2.length; j++) {
+      temp.push(arr2[j][i]);
+    }
+    arr2Column.push(temp);
+  }
+
+  for(let k = 0; k < arr1.length; k++) {
+    let a = [];
+    for(let l = 0; l < arr2Column.length; l++) {
+      let count = 0;
+      for(let m = 0; m < arr2Column[l].length; m++) {
+        count += (arr1[k][m] * arr2Column[l][m]);
+      }
+      a.push(count);
+    }
+    result.push(a);
+  }
+
+  return result
+}
