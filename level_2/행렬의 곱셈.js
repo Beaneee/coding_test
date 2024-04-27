0e300c4c439d832343d81f799930644598769c1d
종빈 <whddkrak@gmail.com>
2024-04-27 16:51:50 +0900

    [level2] 행렬의 곱셈 refactor

--- level_2/행렬의 곱셈.js
+++ level_2/행렬의 곱셈.js
@@ -1,15 +1,15 @@
-function solution(arr1, arr2) {
-  //arr2를 변환 시키자.
-  let arr2Column = [];
-  let result = [];
+/**
+ * 열을 추출하면 배열로 반환
+ * @param {*} matrix 
+ */
+function transposeMatrix(matrix) {
+  return matrix[0].map((_, index) => matrix.map(row => row[index]))
+}
 
-  for(let i = 0; i < arr2[0].length; i++) {
-    let temp = [];
-    for(let j = 0; j < arr2.length; j++) {
-      temp.push(arr2[j][i]);
-    }
-    arr2Column.push(temp);
-  }
+
+function solution(arr1, arr2) {
+  const arr2Column = transposeMatrix(arr2);
+  let result = [];
 
   for(let k = 0; k < arr1.length; k++) {
     let a = [];
