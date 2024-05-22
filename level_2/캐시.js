e25712d232b9634272b130599b808a10734fe993
종빈 <whddkrak@gmail.com>
2024-05-22 21:31:13 +0900

    [level2] 캐시 #3

--- level_2/캐시.js
+++ level_2/캐시.js
@@ -2,20 +2,18 @@ function solution(cacheSize, cities) {
   let visitedArr = [];
   let time = 0;
   for(let i = 0; i < cities.length; i++) {
-
-    if(visitedArr.length === cacheSize) {
-      //방문한적이 있다면
-      if(visitedArr.includes(cities[i])) {
-        visitedArr = visitedArr.filter(v => v !== cities[i]);
-        visitedArr.unshift(cities[i]);
-        time += 1;
-      } else {
-        visitedArr.pop();
-        visitedArr.push(cities[i]);
-        time += 5;
-      }
-    } else {
+    //만약 방문했던 도시가 있다면, 제거하고 뒤에 넣는다.
+    if(visitedArr.includes(cities[i])) {
+      visitedArr = visitedArr.filter(v => v !== cities[i])
       visitedArr.push(cities[i]);
+      time += 1;
+    } else {
+      //만약 방문했던 도시가 없다면, 가장 먼저들어온 것(오래된것)부터 빼고 뒤에 넣는다.
+      if(visitedArr.length === cacheSize) {
+        visitedArr.shift();
+      }
+      visitedArr.push(cities[i]);
+      time += 5;
     }
   }
   return time
