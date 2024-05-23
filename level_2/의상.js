e2ecf010aa66ab7c97ee38bfed680d8b2b09bf08
종빈 <whddkrak@gmail.com>
2024-05-23 23:54:00 +0900

    [level2] 의상 #2

--- level_2/의상.js
+++ level_2/의상.js
@@ -1,15 +1,16 @@
 function solution(clothes) {
   let hash = {};
-  //key 생성
-  for(let i = 0; i < clothes.length; i++) {
-    const key = clothes[i][1];
-    hash[key] = [];
-  }
-  //value 생성
+  //hash 생성
   for(let i = 0; i < clothes.length; i++) {
     const key = clothes[i][1];
     const value = clothes[i][0];
-    hash[key].push(value);
+
+    if(hash[key]) {
+      hash[key].push(value);
+    } else {
+      hash[key] = [value];
+    }
+    
   }
   console.log(hash);
 }
