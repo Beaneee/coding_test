5ed6ec48911e176522c9951569d8ef89b0621367
종빈 <whddkrak@gmail.com>
2024-05-24 23:02:10 +0900

    [level2] 튜플 #2

--- level_2/튜플.js
+++ level_2/튜플.js
@@ -1,11 +1,22 @@
 function solution(s) {
   let strArr = s.split('},')
-  let arr = [];
-
+  let replaceArr = [];
+  let result = [];
+  //문자열을 배열로 만들고
   for(let i = 0; i < strArr.length; i++) {
     strArr[i] = strArr[i].replace(/{/g, '').replace(/}/g, '').split(',').sort();
-    arr.push(strArr[i])
+    replaceArr.push(strArr[i])
   }
-  arr.sort((a, b) => a.length - b.length);
-  console.log(arr);
-}
+  //길이가 짧은 순으로 sort후
+  replaceArr.sort((a, b) => a.length - b.length);
+
+  for(let j = 0; j < replaceArr.length; j++) {
+    for(let k = 0; k < replaceArr[j].length; k++) {
+      const value = Number(replaceArr[j][k]);
+      if(!result.includes(value)) {
+        result.push(value);
+      }
+    }
+  }
+  return result;
+} 
