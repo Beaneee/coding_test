4d9250183c805284534d9925260e3b98f55ea7bd
종빈 <whddkrak@gmail.com>
2024-06-04 00:51:18 +0900

    [level2] 프로세스 #2

--- level_2/프로세스.js
+++ level_2/프로세스.js
@@ -3,13 +3,22 @@ function solution(priorities, location) {
 
   //hash map으로 data 생성
   for(let i = 0; i < priorities.length; i++) {
-    arr.push({[i] : priorities[i]})
+    arr.push({[i] : priorities[i]});
   } 
-
+  
   let queueList = [];
-  
-  for(let j = 0; j < priorities.length; j++) {
-    queueList.push(arr.shift());
-  }
-  
+
+  //data의 value를 비교하며 queuelist 생성
+  while(arr.length > 0) {
+    const value = arr.shift();
+    
+    if(arr.length > 0 && Object.values(value) < Object.values(arr[0])) {
+      arr.push(value);
+    } else {
+      queueList.push(value);
+    }
+
+  } 
+  //location에 해당하는 key index 찾기
+  return queueList.findIndex(v => Object.keys(v)[0] === String(location));
 }
