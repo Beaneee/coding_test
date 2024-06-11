7663b6ae7b5d276a153a7fcf7b73a1f3d09f75ad
종빈 <whddkrak@gmail.com>
2024-06-11 23:43:02 +0900

    [level1] 정수 제곱근 판별

--- level_1/정수 제곱근 판별.js
+++ level_1/정수 제곱근 판별.js
@@ -1,9 +1,10 @@
 function solution(n) {
   let answer = -1;
 
-  for(let i = 0; i < n; i++) {
+  for(let i = 0; i <= n; i++) {
     if(i * i === n) {
-      answer = (i + 1) * (i + 1)
+      //50000000000000번 까지 돌기전에 답이 나오면 return하는 버릇을 들이자
+      return (i + 1) * (i + 1)
     }
   }
   return answer
