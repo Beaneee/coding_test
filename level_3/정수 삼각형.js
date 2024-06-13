3259488d1c926a73bcd057479bfbd28b29971288
종빈 <whddkrak@gmail.com>
2024-06-13 23:28:57 +0900

    [level3] 정수삼각형 #2

--- level_3/정수 삼각형.js
+++ level_3/정수 삼각형.js
@@ -1,18 +1,28 @@
-function solution(triangle) {
-  const root = triangle[0];
-  let answer = root[0];
-  let compareIdx = 0;
+/**
+ * 완전 탐색
+ */
+ function solution(triangle) {
+  let allPath = [];
+  traverseTriangle(triangle, 0, 0, [], allPath);
+  let allPathSum = setArrElementSum(allPath);
+  return Math.max(...allPathSum)
+}
 
-  //이미 root를 가지고 있기 때문에 1부터 시작
-  for(let i = 1; i < triangle.length; i++) {
-    //이전 최대값 idx와 idx + 1의 최대값을 찾고
-    const max = Math.max(triangle[i][compareIdx], triangle[i][compareIdx + 1]);
-    //최대값의 idx를 갱신하고
-    const maxIdx = triangle[i].findIndex(v => v === max);
-    compareIdx = maxIdx
-    //값을 더한다.
-    answer += max
+/**
+ * 삼각형 순회
+ * @param {*} triangle 
+ * @param {*} row 
+ * @param {*} col 
+ * @param {*} path 
+ */
+const traverseTriangle = (triangle, row, col, path, allPath) => {
+  path.push(triangle[row][col]);
+
+  if(row === triangle.length - 1) {
+    allPath.push([...path])
+    return
   }
 
-  return answer;
+  traverseTriangle(triangle, row + 1, col, [...path], allPath)
+  traverseTriangle(triangle, row + 1, col + 1, [...path], allPath)
 }
