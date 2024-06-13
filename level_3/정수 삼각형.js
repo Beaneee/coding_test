1eeb6c754dec808a46fe4d395f6ab99375ef895a
종빈 <whddkrak@gmail.com>
2024-06-13 23:39:40 +0900

    [level3] 정수 삼각형 #3

--- level_3/정수 삼각형.js
+++ level_3/정수 삼각형.js
@@ -1,28 +1,22 @@
-/**
- * 완전 탐색
- */
- function solution(triangle) {
-  let allPath = [];
-  traverseTriangle(triangle, 0, 0, [], allPath);
-  let allPathSum = setArrElementSum(allPath);
-  return Math.max(...allPathSum)
+function solution(triangle) {
+  return traverseTriangle(triangle, 0, 0);
 }
 
 /**
- * 삼각형 순회
+ * 삼각형 순회 및 최대 경로 합 계산
  * @param {*} triangle 
  * @param {*} row 
  * @param {*} col 
- * @param {*} path 
  */
-const traverseTriangle = (triangle, row, col, path, allPath) => {
-  path.push(triangle[row][col]);
-
-  if(row === triangle.length - 1) {
-    allPath.push([...path])
-    return
+const traverseTriangle = (triangle, row, col) => {
+  // Base case: 가장 아래 행에 도달한 경우
+  if (row === triangle.length - 1) {
+    return triangle[row][col];
   }
 
-  traverseTriangle(triangle, row + 1, col, [...path], allPath)
-  traverseTriangle(triangle, row + 1, col + 1, [...path], allPath)
+  // 현재 위치에서 아래로 내려가는 두 경로의 합 중 최대값을 선택하여 리턴
+  let leftPathSum = traverseTriangle(triangle, row + 1, col);
+  let rightPathSum = traverseTriangle(triangle, row + 1, col + 1);
+
+  return triangle[row][col] + Math.max(leftPathSum, rightPathSum);
 }
