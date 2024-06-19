ae844124ab9eb11ef5f563ef5a105429e4a0b61f
종빈 <whddkrak@gmail.com>
2024-06-19 22:06:15 +0900

    [level2] 게임 맵 최단거리 #2

--- level_2/게임 맵 최단거리.js
+++ level_2/게임 맵 최단거리.js
@@ -1,55 +1,37 @@
-class Queue {
-  constructor() {
-    this.storage = {}
-    this.front = 0;
-    this.rear = 0;
-  }
-
-  //storage 크기
-  size() {
-    //아무것도 없을 때
-    if(this.storage[this.rear] === undefined) return 0
-    return (this.rear - this.front) + 1
-  }
-
-  deleteValue() {
-    //앞에 있는 데이터가 뭔지 저장하고
-    temp = this.storage[this.front];
-    //storage에서 삭제하고
-    delete this.storage[this.front];
-    return temp
-  }
-
-  //데이터 추가
-  push(value) {
-    //초기일 경우
-    if(this.size() === 1) {
-      this.storage['0'] = value;
-      return
-    }
-    this.rear += 1;
-    this.storage[this.rear] = value;
-  }
-
-  //데이터 추출
-  shift() {
-    let temp = null;
-    //size가 1일 경우
-    if(this.size() === 1) {
-      temp = deleteValue();
-      //모두 제거했으니까 초기화
-      this.front = 0;
-      this.rear = 0;
-    } else { //size가 1이상 일 경우
-      temp = deleteValue();
-      //front를 1 늘려야함
-      this.front += 1;
-    }
-    return temp;
-  }
-}
-
 function solution(maps) {
-  var answer = 0;
-  return answer;
+  let queue = [];
+  const m = maps[0].length; //x
+  const n = maps.length; //y
+  const goalX = m - 1;
+  const goalY = n - 1;
+  //상 하 좌 우
+  const dx = [-1, 1, 0, 0]; 
+  const dy = [0, 0, 1, -1];
+
+  queue.push([0, 0, 1]);
+
+  while(queue.length > 0) {
+    const [currY, currX, move] = queue.shift();
+
+    if(currY === goalY && currX === goalX) return move;
+
+    for(let i = 0; i < 4; i++) {
+      //이동한 좌표
+      const moveX = currX + dx[i];
+      const moveY = currY + dy[i];
+      //움직인 좌표가 유효한지
+      const isValidMoveXY = moveX >= 0 && moveY >= 0;
+      //움직인 좌표가 maps안에 있는지
+      const isContainMoveXY = moveX < m && moveY < n;
+      //범위내에 있다면
+      if(isValidMoveXY && isContainMoveXY && maps[moveY][moveX] === 1) {
+        queue.push([moveY, moveX, move + 1]);
+        //이미 방문했으니 못가게 만들어야 한다.
+        maps[moveY][moveX] = 0;
+      }
+
+    }
+  }
+
+  return -1
 }
