f265c7ce014a1ac81aa169736ac49362c1d45511
종빈 <whddkrak@gmail.com>
2024-06-24 16:23:31 +0900

    [level2] 뒤에 있는 큰 수 찾기 #4

--- level_2/뒤에 있는 큰 수 찾기.js
+++ level_2/뒤에 있는 큰 수 찾기.js
@@ -1,15 +1,24 @@
 function solution(numbers) {
-  let answer = [];
-  let targetIdx = 0;
-  while (numbers.length - 1 >= targetIdx) {
-    //비교하려는 숫자
-    let compareNumber = numbers[targetIdx];
-    //비교하려는 숫자보다 큰 수
-    //filter를 쓰지말고 find로 바꾸자.
-    //최악의 경우 O(n) (모든 요소를 검사할 수 있지만, 첫 번째 요소를 찾으면 즉시 종료).
-    const lageNumber = numbers.find((num, idx) => idx > targetIdx && num > compareNumber);
-    answer.push(lageNumber ?? -1);
-    targetIdx += 1;
+  let answer = new Array(numbers.length).fill(-1); // 초기화
+  let stack = [];
+
+  for (let i = numbers.length - 1; i >= 0 ;i--) {
+    const currNumber = numbers[i];
+    console.log('currNumber', currNumber)
+    //만약 stack의 가장 마지막 요소가 currNumber보다 작다면 pop()
+    //예를 들어, 3 - 5중에서 5가 더 크므로 3을 빼준다.
+    while (stack.length > 0 && stack[stack.length - 1] <= currNumber) {
+      stack.pop();
+    }
+
+    if(stack.length > 0) {
+      answer[i] = stack[stack.length - 1]
+    }
+
+    //그리고 currNumber를 push해준다.
+    //현재 flow에서 가장 큰 값은 stack의 가장 마지막 요소이다.
+    stack.push(currNumber);
   }
+
   return answer;
 }
