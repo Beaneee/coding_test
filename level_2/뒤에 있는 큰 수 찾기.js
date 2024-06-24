e500a12384f8fcc347cd0f73d85871774587dca4
종빈 <whddkrak@gmail.com>
2024-06-24 14:57:02 +0900

    [level2] 뒤에 있는 큰 수 찾기 #2

--- level_2/뒤에 있는 큰 수 찾기.js
+++ level_2/뒤에 있는 큰 수 찾기.js
@@ -4,11 +4,11 @@ function solution(numbers) {
   while (numbers.length > 0) {
     //비교하려는 숫자
     let compareNumber = numbers[0];
-    //비교하려는 숫자보다 큰 수 list
-    const lageNumbers = numbers.filter(num => num > compareNumber);
-
-    answer.push(lageNumbers.length > 0 ? lageNumbers[0] : -1);
-
+    //비교하려는 숫자보다 큰 수
+    //filter를 쓰지말고 find로 바꾸자.
+    //최악의 경우 O(n) (모든 요소를 검사할 수 있지만, 첫 번째 요소를 찾으면 즉시 종료).
+    const lageNumber = numbers.find(num => num > compareNumber);
+    answer.push(lageNumber ?? -1);
     numbers.splice(0, 1);
   }
   return answer;
