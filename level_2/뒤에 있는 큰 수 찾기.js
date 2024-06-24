fbd3fc3ea7a26a138f0dad171ab0c60c080a10f6
종빈 <whddkrak@gmail.com>
2024-06-24 15:03:23 +0900

    [level2] 뒤에 있는 큰 수 찾기 #3

--- level_2/뒤에 있는 큰 수 찾기.js
+++ level_2/뒤에 있는 큰 수 찾기.js
@@ -1,15 +1,15 @@
 function solution(numbers) {
   let answer = [];
-
-  while (numbers.length > 0) {
+  let targetIdx = 0;
+  while (numbers.length - 1 >= targetIdx) {
     //비교하려는 숫자
-    let compareNumber = numbers[0];
+    let compareNumber = numbers[targetIdx];
     //비교하려는 숫자보다 큰 수
     //filter를 쓰지말고 find로 바꾸자.
     //최악의 경우 O(n) (모든 요소를 검사할 수 있지만, 첫 번째 요소를 찾으면 즉시 종료).
-    const lageNumber = numbers.find(num => num > compareNumber);
+    const lageNumber = numbers.find((num, idx) => idx > targetIdx && num > compareNumber);
     answer.push(lageNumber ?? -1);
-    numbers.splice(0, 1);
+    targetIdx += 1;
   }
   return answer;
 }
