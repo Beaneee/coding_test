84e65fef3e6764415993fad5df268883c1bf7355
종빈 <whddkrak@gmail.com>
2024-06-26 11:34:04 +0900

    [level2] 땅따먹기 #2

--- level_2/땅따먹기.js
+++ level_2/땅따먹기.js
@@ -1,13 +1,12 @@
 function solution(land) {
-  let sum = Math.max(...land[0]);
-  let maxIdx = land[0].findIndex(v => v === Math.max(...land[0]));
-
+  //dp로 풀어야한다.
+  //이전 값을 바라보고 값을 구해야한다.
   for (let i = 1; i < land.length; i++) {
-    land[i].splice(maxIdx, 1);
-    const max = Math.max(...land[i]);
-    maxIdx = land[i].findIndex(v => v === Math.max(...land[i]));
-
-    sum += max;
+    land[i][0] += Math.max(land[i-1][1], land[i-1][2], land[i-1][3]);
+    land[i][1] += Math.max(land[i-1][0], land[i-1][2], land[i-1][3]);
+    land[i][2] += Math.max(land[i-1][0], land[i-1][1], land[i-1][3]);
+    land[i][3] += Math.max(land[i-1][0], land[i-1][1], land[i-1][2]);
   }
-  return sum
+
+  return Math.max(...land[land.length - 1]);
 }
