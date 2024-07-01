9c2f8f6f4fbb631a6ade2b58d145754d4f1999bc
종빈 <whddkrak@gmail.com>
2024-07-01 10:46:29 +0900

    [level2] 롤케이크 자르기 #2

--- level_2/롤케이크 자르기.js
+++ level_2/롤케이크 자르기.js
@@ -1,22 +1,41 @@
+//철수는 롤케이크를 '두 조각'으로 잘라서 동생과 한 조각씩 나눠 먹으려고 합니다.
 function solution(topping) {
-  let spliceNum = 1;
-  spliceTopping(spliceNum, topping);
-}
+  let sliceTopping = new Map();
+  let baseTopping = new Map();
+  let answer = 0;
 
-const spliceTopping = (spliceNum, arr) => {
-  if(spliceNum === arr.length) return
-  let copyArr = [];
-  let a = [];
-  let b = [];
-
-  for (let i = 0; i < arr.length; i++) {
-    copyArr = arr.slice();
-    b = [...new Set(copyArr.splice(i, spliceNum))];
-    a = [...new Set(copyArr)];
-
-    if(a.length === b.length) {
-      console.log(a, b);
+  //topping의 개수를 모두 넣어주고
+  for (const t of topping) {
+    if (baseTopping.get(t)) {
+      baseTopping.set(t, baseTopping.get(t) + 1);
+      continue
     }
+    baseTopping.set(t, 1);
   }
-  spliceTopping(spliceNum + 1, arr);
+
+  //왼쪽부터 커팅을 했을 경우 size비교
+  for (const t of topping) {
+
+    if(baseTopping.size === sliceTopping.size) {
+      answer += 1;
+    }
+
+    if(baseTopping.has(t)) {
+      baseTopping.set(t, baseTopping.get(t) - 1);
+
+      //더이상 토핑이 없으면 삭제해주기
+      if (baseTopping.get(t) === 0) {
+        baseTopping.delete(t);
+      }
+    }
+
+    if(sliceTopping.get(t)) {
+      sliceTopping.set(t, sliceTopping.get(t) + 1);
+      continue
+    }
+    sliceTopping.set(t, 1);
+  }
+
+  return answer
 }
+
