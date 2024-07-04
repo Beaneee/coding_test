b4d756a2f21601c1749a40e20cdf43ef32581504
종빈 <whddkrak@gmail.com>
2024-07-04 15:45:11 +0900

    [level3] 최고의 집합 #3

--- level_3/최고의 집합.js
+++ level_3/최고의 집합.js
@@ -2,13 +2,11 @@ function solution(n, s) {
   if(n > s) return [-1];
   let answer = [];
 
-  while (true) {
+  while (n > 0) {
     let value = Math.floor(s / n);
     answer.push(value);
     s = s - value;
     n = n - 1;
-
-    if(s === 0) break;
   }
 
   return  answer
