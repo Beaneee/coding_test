2988d1ad1d1092d47debfb259bc2c0bc026173cb
종빈 <whddkrak@gmail.com>
2024-07-08 15:41:59 +0900

    [level3] 등굣길 #3 (런타임 에러)

--- level_3/등굣길.js
+++ level_3/등굣길.js
@@ -1,49 +1,46 @@
+/**
+ * dynamic programming으로 풀어야 한다. (0,0)에서 시작해서 끝 지점 (m,n)이 고정되어있다.
+ * 직전의 값을 +1 만 해주면 된다.
+ *
+ * @param m
+ * @param n
+ * @param puddles
+ */
 function solution(m, n, puddles) {
-  const min = (m - 1) + (n - 2);
   //우선 m,n만큼 배열을 만들어준다.
   let maps = [];
-
   for (let i = 0; i < n; i++) {
     maps[i] = [];
     for (let j = 0; j < m; j++) {
-      maps[i].push(1);
+      maps[i].push(0);
     }
   }
-
   //비온 곳은 못가게 하고
   for (let i = 0; i < puddles.length; i++) {
     const [x, y] = puddles[i];
-    maps[x-1][y-1] = 0;
+    maps[x-1][y-1] = -1;
   }
 
-  let queue = [];
-  const dx = [1, 0];
-  const dy = [0 , 1];
+  //탐색 시작
+  maps[0][0] = 1;
 
-  queue.push([0, 0, 0]);
-
-  while (queue.length > 0) {
-    const [currY, currX, move] = queue.shift();
-
-    if(move === min) {
-      console.log(currY, currX);
-    }
-
-    for (let i = 0; i < 2; i++) {
-
-      const moveX = currX + dx[i];
-      const moveY = currY + dy[i];
-      const isContainMoveXY = moveX < m && moveY < n;
-
-      if(isContainMoveXY && maps[moveY][moveX] === 1) {
-        queue.push([moveY, moveX, move + 1]);
-        maps[moveY][moveX] = 0;
+  //이동하고
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < m; j++) {
+      if(maps[i][j] === -1) {
+        maps[i][j] = 0;
+        continue; //웅덩이 좌표는 통과
+      }
+      //y좌표 이동했을 경우
+      if(i > 0) {
+        maps[i][j] += maps[i - 1][j];
+      }
+      //x좌표 이동했을 경우
+      if(j > 0) {
+        maps[i][j] += maps[i][j - 1];
       }
     }
-    console.log(maps);
   }
-  //이동하고 -> 어떻게???
-
   //그 수 만큼 움직인 곳의 개수를 구한다
-
+  return maps[n - 1][m - 1] % 1000000007;
 }
