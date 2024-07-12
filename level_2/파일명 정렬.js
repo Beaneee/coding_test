6b246404e25f2784924a21ad24a77ce40e95e82c
종빈 <whddkrak@gmail.com>
2024-07-12 14:33:53 +0900

    [level2] 파일명 정렬 #2

--- level_2/파일명 정렬.js
+++ level_2/파일명 정렬.js
@@ -1,11 +1,23 @@
 function solution(files) {
-
+  let arr = [];
   //head, number, tail로 나눠보자
   files.forEach(file => {
     //number idx 찾기
     const strArr = splitStr(file);
-    console.log(strArr);
+    arr.push(strArr)
   })
+  //먼저 head로 정렬
+  const isSameHead = arr.every(v => v[0].toUpperCase() === arr[0][0].toUpperCase());
+  isSameHead ? arr : arr.sort((a, b) => a[0].localeCompare(b[0]));
+  if(!isSameHead) return arr.map(v => v.join(''))
+
+  //head가 같다면 number로 정렬
+  const isSameNumber = arr.every(v => v[1] === arr[0][1]);
+  isSameNumber ? arr : arr.sort((a, b) => a[1] - b[1]);
+  if(!isSameHead) return arr.map(v => v.join(''))
+
+  //둘 다 차이가 없다면, 순서 유지
+  return arr.map(v => v.join(''))
 }
 
 /**
@@ -42,7 +54,8 @@ const splitStr = (file) => {
   return [
     file.slice(0, startNumIdx),
     file.slice(startNumIdx, endNumIdx + 1),
-    file.slice(endNumIdx + 1, endNumIdx + 2) === ' ' ? ' ' : file.slice(endNumIdx + 1)
+    // file.slice(endNumIdx + 1, endNumIdx + 2) === ' ' ? ' ' : file.slice(endNumIdx + 1)
+    file.slice(endNumIdx + 1)
   ]
 }
 
