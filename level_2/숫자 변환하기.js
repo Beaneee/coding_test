b9a2d14b71f8adeda18149b05fca55e3adaf3a02
종빈 <whddkrak@gmail.com>
2024-07-16 17:38:17 +0900

    [level2] 숫자 변환하기 #2

--- level_2/숫자 변환하기.js
+++ level_2/숫자 변환하기.js
@@ -1,31 +1,26 @@
 /* dp로 풀어야한다. bfs는 shift, push 때문에 시간 초과가 발생한다. */
 function solution(x, y, n) {
-  //애초에 x,y가 같은 경우
   if (x === y) return 0;
 
-  let arr = [];
-  //y길이 만큼 배열 생성
-  for (let i = 0; i <= y; i++) {
-    arr.push(-1);
-  }
+  let dp = new Array(y + 1).fill(Infinity); // y+1 길이 만큼 배열 생성
+  dp[x] = 0;
 
-  arr[x] = 0;
+  for (let i = x; i <= y; i++) {
+    if (dp[i] === Infinity) continue; // 현재 위치에서 변환 불가능한 경우
 
-  for (let j = x; j <= y; j++) {
-    if (arr[j] === -1) continue; // 현재 위치에서 변환 불가능한 경우
-
-    if (j * 2 <= y && arr[j * 2] === -1) {
-      arr[j * 2] = arr[j] + 1;
+    if (i + n <= y) {
+      dp[i + n] = Math.min(dp[i + n], dp[i] + 1);
     }
 
-    if (j * 3 <= y && arr[j * 3] === -1) {
-      arr[j * 3] = arr[j] + 1;
+    if (i * 2 <= y) {
+      dp[i * 2] = Math.min(dp[i * 2], dp[i] + 1);
     }
 
-    if (j + n <= y && arr[j + n] === -1) {
-      arr[j + n] = arr[j] + 1;
+    if (i * 3 <= y) {
+      dp[i * 3] = Math.min(dp[i * 3], dp[i] + 1);
     }
   }
-  return arr[y];
+
+  return dp[y] === Infinity ? -1 : dp[y];
 }
 
