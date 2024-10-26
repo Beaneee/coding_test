8f4df6f1a24d8dd4d1388f3fccf1cde03be8293b
종빈 <whddkrak@gmail.com>
2024-10-26 15:45:41 +0900

    feat: 배열 제어하기

--- 코딩테스트_합격자되기_자바스크립트/배열/배열 제어하기.js
+++ 코딩테스트_합격자되기_자바스크립트/배열/배열 제어하기.js
@@ -5,6 +5,10 @@
 
 const removeDuplicate = (arr) => {
   // 중복값을 제거
-
+  const removeDuplicateArr = [...new Set(arr)];
   // 내림차순으로 정렬
+  return removeDuplicateArr.sort((a, b) => b - a);
 }
+
+console.log(removeDuplicate([4,2,2,1,3,4]));
+console.log(removeDuplicate([2,1,1,3,2,5,4]));
