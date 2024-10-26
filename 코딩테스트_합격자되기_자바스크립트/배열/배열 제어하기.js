918b3d3ef1341239402f29e021ef301b48fde2c3
종빈 <whddkrak@gmail.com>
2024-10-26 17:02:30 +0900

    feat: 행렬의 곱셈 pseudo code

--- 코딩테스트_합격자되기_자바스크립트/배열/배열 제어하기.js
+++ 코딩테스트_합격자되기_자바스크립트/배열/배열 제어하기.js
@@ -2,7 +2,6 @@
  * 정수 배열을 하나 받습니다. 배열의 중복값을 제거하고 배열 데이터를 내림차순으로 정렬해서 반환하는 solution()
  * 함수를 구현하세요.
  */
-
 const removeDuplicate = (arr) => {
   // 중복값을 제거
   const removeDuplicateArr = [...new Set(arr)];
