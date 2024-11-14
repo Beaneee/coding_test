b6ed94bda67744a13ddd8b3638b19b072806211c
종빈 <whddkrak@gmail.com>
2024-11-14 17:18:05 +0900

    feat: 괄호 짝 맞추기

--- 코딩테스트_합격자되기_자바스크립트/스택/괄호 짝 맞추기.js
+++ 코딩테스트_합격자되기_자바스크립트/스택/괄호 짝 맞추기.js
@@ -1,6 +1,25 @@
 const solution = (s) => {
+  const strArr = s.split('');
+  let stack = [];
 
+  strArr.forEach(str => {
+
+    if(str === '(') {
+      if(stack.length === strArr.length) return false; // isFull
+      stack.push(str);
+    }
+
+    if(str === ')') {
+      if(stack.length === 0) return false; // isEmpty
+      stack.pop();
+    }
+
+  })
+
+  return stack.length === 0
 }
 
 console.log(solution('(())()'));
 console.log(solution('((())()'));
+console.log(solution('()())'));
+console.log(solution('(((((('));
