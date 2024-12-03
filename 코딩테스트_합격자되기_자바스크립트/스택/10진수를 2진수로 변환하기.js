14535df777a9f7475e0c2f5ac24ae3f3c1729c1d
종빈 <whddkrak@gmail.com>
2024-12-03 23:31:35 +0900

    feat: 10진수를 2진수로 변환하기

--- 코딩테스트_합격자되기_자바스크립트/스택/10진수를 2진수로 변환하기.js
+++ 코딩테스트_합격자되기_자바스크립트/스택/10진수를 2진수로 변환하기.js
@@ -1,7 +1,22 @@
 const solution = (decimal) => {
+  const stack = [];
+  let num = decimal;
 
+  while (num > 1) {
+    const q = parseInt(num / 2);
+    const rest = parseInt(num % 2);
+
+    stack.push(rest);
+    if(q === 1) {
+      stack.push(q);
+    }
+    num = q;
+
+  }
+  return Number(stack.join(''))
 }
 
 console.log(solution(10));
-console.log(solution(27));
+console.log(solution(100));
 console.log(solution(12345));
+
