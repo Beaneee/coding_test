9e355b9412fed34d7962f81eb605671345b6d3a3
김종빈 <been.kim@twolinecode.com>
2024-12-04 10:48:58 +0900

    feat: 괄호 회전하기 pseudo code

---
+++ 코딩테스트_합격자되기_자바스크립트/스택/괄호 회전하기.js
@@ -0,0 +1,18 @@
+const solution = (s) => {
+  console.log(s);
+
+  // s의 길이 만큼 pop / push를 진행한다.
+
+  // 괄호 짝이 맞는지 검사한다.
+
+  // 맞으면 result를 하나씩 더해준다.
+
+  return 0;
+}
+
+
+
+console.log(solution('[](){}'));
+console.log(solution('}]()[{'));
+console.log(solution('[)(]'));
+console.log(solution('}}}'));
