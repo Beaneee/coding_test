9618bb700d1937a23176deb970b728b1152350ba
종빈 <whddkrak@gmail.com>
2024-12-07 16:50:58 +0900

    feat: 주식가격 실패

--- 코딩테스트_합격자되기_자바스크립트/스택/주식가격.js
+++ 코딩테스트_합격자되기_자바스크립트/스택/주식가격.js
@@ -2,14 +2,25 @@ const solution = (p) => {
   const stack = [];
   const result = Array(p.length).fill(0);
   // p를 순회하면서
+  for (let i = 0; i < p.length; i++) {
+    // stack에 있는 마지막 index의 값으로 이전 가격을 비교한다.
 
-  // stack에 index를 저장한다.
+    // 만약 현재 가격이 이전 가격보다 작다면
+    if (stack.length > 0 && p[stack[stack.length - 1]] > p[i]) {
+      const idx = stack.pop();
+      result[idx] = i - idx;
+    }
 
-  // stack에 있는 마지막 index의 값으로 이전 가격을 비교한다.
+    // 그렇지 않다면
+    // stack에 index를 저장한다.
+    stack.push(i);
+  }
 
-  // 만약 현재 가격이 이전 가격보다 작다면
-
-  // 그렇지 않다면
+  // stack에 남아있는 index는 계속해서 주식가격이 증가 또는 유지 되었다는 의미이다.
+  // 나머지를 계산한다.
+  for (const idx of stack) {
+    result[idx] = p.length - idx - 1;
+  }
 
   // result를 반환한다.
   return result;
