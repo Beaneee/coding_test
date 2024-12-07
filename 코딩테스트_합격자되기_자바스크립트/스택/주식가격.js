77d3be0055968a3e3e8ec71c6509fa5c2876e845
종빈 <whddkrak@gmail.com>
2024-12-07 16:37:55 +0900

    feat: 주식가격 pseudo code

---
+++ 코딩테스트_합격자되기_자바스크립트/스택/주식가격.js
@@ -0,0 +1,18 @@
+const solution = (p) => {
+  const stack = [];
+  const result = Array(p.length).fill(0);
+  // p를 순회하면서
+
+  // stack에 index를 저장한다.
+
+  // stack에 있는 마지막 index의 값으로 이전 가격을 비교한다.
+
+  // 만약 현재 가격이 이전 가격보다 작다면
+
+  // 그렇지 않다면
+
+  // result를 반환한다.
+  return result;
+}
+
+console.log(solution([1, 2, 3, 2, 3]));
