6b051f9efbccc99fb5a4fd70daf6cd5f9cda4e7b
종빈 <whddkrak@gmail.com>
2024-12-07 15:44:02 +0900

    feat: 짝지어 제거하기 예외 케이스

--- 코딩테스트_합격자되기_자바스크립트/스택/짝지어 제거하기.js
+++ 코딩테스트_합격자되기_자바스크립트/스택/짝지어 제거하기.js
@@ -2,25 +2,24 @@ const solution = (s) => {
   const stack = [];
   // 문자열을 순회하면서
   for (let i = 0; i < s.length; i++) {
-    // 문자를 stack에 넣고
-    stack.push(s[i]);
-
-    // 문자를 하나 뽑아서 다음 문자열과 같은지 비교한다.
-    const sliceStr = s.slice(i + 1, i + 2);
-    // 같다면
-    if(sliceStr && s[i] === sliceStr) {
-      // 두 문자열을 뽑아주고
+    // 만약 stack에 마지막 요소와 현재 문자와 같다면
+    // isEmpty를 검사하고
+    if(stack.length > 0 && s[i] === stack[stack.length - 1]) {
+      // pop 진행
       stack.pop();
-      stack.pop();
-      // 다다음 문자열로 넘어간다. (이미 같은 문자열은 뽑았다는 가정하에)
-      i++
+    } else {
+      // isFull
+      if (stack.length === s.length) return;
+      // 그렇지 않다면 push
+      stack.push(s[i]);
     }
-    // 아니라면 유지한다.
   }
-
   // stack이 비어 있으면 1 아니면 0을 return
   return stack.length === 0 ? 1 : 0
 }
 
 console.log(solution('baabaa'));
 console.log(solution('cdcd'));
+console.log(solution('aaccbccb'));
+
+// stack 자료구조는 현재와 이전 혹은 다음 요소를 비교하는 상황에서 굉장히 유용하다.
