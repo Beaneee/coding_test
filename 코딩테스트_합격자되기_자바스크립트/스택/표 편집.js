c8f5758b6526a5718f93c841808dd77500fa7d1d
종빈 <whddkrak@gmail.com>
2024-12-18 18:19:39 +0900

    feat: 표 편집 pseudo code

---
+++ 코딩테스트_합격자되기_자바스크립트/스택/표 편집.js
@@ -0,0 +1,13 @@
+const solution = (n, k, cmd) => {
+  // 길이가 n인 배열 생성
+
+  // cmd 명령대로 수행할 함수
+
+  // switch
+  // U X -> k + x;
+  // D X -> k - x;
+  // C -> split k, push stack;
+  // Z -> pop stack, pop된 value를 다시 배열에 삽입;
+
+  // stack에 있는 key의 value만 O로 표시해서 return
+}
