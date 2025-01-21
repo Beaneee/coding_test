f609c8cbc2b2b1c164a822c4ae239fafe8047d40
종빈 <whddkrak@gmail.com>
2025-01-21 23:31:26 +0900

    feat: 기능개발

--- 코딩테스트_합격자되기_자바스크립트/큐/기능개발.js
+++ 코딩테스트_합격자되기_자바스크립트/큐/기능개발.js
@@ -1,9 +1,14 @@
 const solution = (progresses, speeds) => {
   // 각 progress마다 개발이 언제 완료되는지 알아야한다.
   // (100 - progress) / speeds[idx]
+  const restDate = progresses.map((progress, idx) => {
+    return Math.ceil((100 - progress) / speeds[idx])
+  })
 
-  // 100을 초과하는 시점이 개발이 완료됐다는 의미이다.
+  const queue = [];
   // 개발이 완료된 front부터 차례대로 뽑는다.
+  // front를 먼저 뽑고, next가 front 보다 크면 뽑는다.
+  // next가 front보다 작으면 뽑지 않는다.
 
   // 뽑힌 수를 result에 push한다.
 
