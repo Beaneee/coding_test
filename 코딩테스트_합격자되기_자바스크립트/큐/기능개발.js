16dfae05d55627eebe87aa508238d8388ca4cae7
종빈 <whddkrak@gmail.com>
2025-01-21 23:25:05 +0900

    feat: 기능개발 pseudo code

---
+++ 코딩테스트_합격자되기_자바스크립트/큐/기능개발.js
@@ -0,0 +1,12 @@
+const solution = (progresses, speeds) => {
+  // 각 progress마다 개발이 언제 완료되는지 알아야한다.
+  // (100 - progress) / speeds[idx]
+
+  // 100을 초과하는 시점이 개발이 완료됐다는 의미이다.
+  // 개발이 완료된 front부터 차례대로 뽑는다.
+
+  // 뽑힌 수를 result에 push한다.
+
+  // result를 return
+
+}
