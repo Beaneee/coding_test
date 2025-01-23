7be70546a1001ba27c8d23248098ebdc28c3fb03
종빈 <whddkrak@gmail.com>
2025-01-23 18:47:29 +0900

    feat: 기능개발

--- 코딩테스트_합격자되기_자바스크립트/큐/기능개발.js
+++ 코딩테스트_합격자되기_자바스크립트/큐/기능개발.js
@@ -1,31 +1,31 @@
 const solution = (progresses, speeds) => {
-
-  // 각 progress마다 개발이 언제 완료되는지 알아야한다.
+  // 각 작업이 완료되는데 필요한 날짜 계산
   const restDate = progresses.map((progress, idx) => {
-    return Math.ceil((100 - progress) / speeds[idx])
-  })
+    return Math.ceil((100 - progress) / speeds[idx]);
+  });
 
   const result = [];
+  let front = restDate[0]; // 첫 번째 작업 기준
+  let count = 1; // 첫 번째 작업은 무조건 포함
 
-  let front = 0;
-
-  // front를 먼저 뽑고,
-  let count = 1;
-
-  // 개발이 완료된 front부터 차례대로 뽑는다.
-  for (let i = 0; i < restDate.length; i++) {
-
-    if(front >= restDate[i]) {
-      // next가 front 보다 크면 뽑는다.
+  for (let i = 1; i < restDate.length; i++) {
+    if (restDate[i] <= front) {
+      // 현재 작업이 front 기준으로 함께 배포 가능
       count++;
     } else {
-      // next가 front보다 작으면 뽑지 않는다.
-      count = 1;
+      // 새로운 배포 시작
+      // 새로운 그룹이 시작될 때만 호출
+      result.push(count);
+      count = 1; // 초기화
+
+      // front를 기준으로 봐야하는데 조건문 밖에서 front를 초기화해주면 항상 초기화가 되기 때문에 문제가 된다.
+      front = restDate[i]; // 새로운 기준 작업
     }
-    // 뽑힌 수를 result에 push한다.
-    // 언제 푸시를 하지?
-    front = restDate[i];
   }
 
+  // 마지막 작업 그룹은 "다음 그룹"이 없기 때문에 추가되지 않는다.
+  // 마지막 배포 처리
+  result.push(count);
+
   return result;
-}
+};
