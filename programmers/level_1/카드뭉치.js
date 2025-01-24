e180bc31aa035066b87195dad37d38955b2536f8
종빈 <whddkrak@gmail.com>
2025-01-24 17:49:01 +0900

    feat: 카드뭉치

--- programmers/level_1/카드뭉치.js
+++ programmers/level_1/카드뭉치.js
@@ -1,32 +1,16 @@
 function solution(cards1, cards2, goal) {
-  const result = [];
 
   for (const el of goal) {
-    let text = '';
 
     if(cards1[0] === el) {
-      text = cards1.shift();
+      cards1.shift();
+    } else if(cards2[0] === el) {
+      cards2.shift();
+    } else {
+      return 'No'
     }
 
-    if(cards2[0] === el) {
-      text = cards2.shift();
-    }
-
-    if(!text) continue;
-    result.push(text);
   }
 
-  // result의 길이와 goal의 길이가 다르다면 No!
-  if(result.length !== goal.length) return 'No'
-
-  let answer = 'Yes';
-
-  // 모두 사용하는게 아니라 만들 수 있으면 Yes 였다.
-  for (let i = 0; i < result.length; i++) {
-    if(result[i] !== goal[i]) {
-      answer = 'No'
-    }
-  }
-
-  return answer;
+  return 'Yes';
 }
