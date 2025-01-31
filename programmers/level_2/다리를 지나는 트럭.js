85085c97357873ac69f0306e804930eca986f2c1
종빈 <whddkrak@gmail.com>
2025-01-31 17:10:23 +0900

    feat: [level2] 다리를 지나는 트럭

--- programmers/level_2/다리를 지나는 트럭.js
+++ programmers/level_2/다리를 지나는 트럭.js
@@ -1,13 +1,56 @@
+class Queue {
+  items = [];
+  front = 0;
+  rear = 0;
+
+  push(item) {
+    this.items.push(item);
+    this.rear++;
+  }
+
+  pop() {
+    return this.items[this.front++];
+  }
+
+  sum() {
+    return this.items.reduce((acc, curr) => acc += curr);
+  }
+
+  length() {
+    return this.items.length;
+  }
+
+  isEmpty() {
+    return this.front === this.rear;
+  }
+}
+
+
 function solution(bridge_length, weight, truck_weights) {
-  // 선입선출이니까 큐를 사용하자.
+  let time = 0;
+  const move_truck = new Queue();
+  const pending_truck = new Queue();
+  pending_truck.items = truck_weights;
 
-  // 대기 트럭이 없어질 동안 반복한다.
+  // 건너는 트럭과 대기중인 트럭이 모두 끝난 상태라면
+  while (move_truck.isEmpty() && pending_truck.isEmpty()) {
+    move_truck.push(pending_truck.pop());
 
-  // 다리를 건너는 트럭들이 무게를 넘기지 않고
+    // 다리를 건너는 트럭들이 무게를 넘기지 않고
+    if(move_truck.sum() < weight) {
 
-  // 다리의 길이를 넘지 않는 만큼 건너는 트럭 배열에 넣고
+    } else {
 
-  // 순차적으로 건넌다.
+    }
 
-  // 소요된 time을 return 한다.
+    // 다리의 길이를 넘지 않는 만큼 건너는 트럭 배열에 넣고
+    if(move_truck.length() < bridge_length) {
+
+    } else {
+
+    }
+
+  }
+
+  return time;
 }
