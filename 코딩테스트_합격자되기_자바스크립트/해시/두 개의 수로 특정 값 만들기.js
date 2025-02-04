da90e3e76184951429dfbbd198ef6540a3f92195
종빈 <whddkrak@gmail.com>
2025-02-04 23:46:50 +0900

    feat: 두 개의 수로 특정 값 만들기 init

---
+++ 코딩테스트_합격자되기_자바스크립트/해시/두 개의 수로 특정 값 만들기.js
@@ -0,0 +1,7 @@
+const solution = (arr, target) => {
+  console.log(arr, target);
+  return true;
+}
+
+console.log(solution([1, 2, 3, 4, 8], 6)); // true
+console.log(solution([2, 3, 5, 9], 10)); // false
