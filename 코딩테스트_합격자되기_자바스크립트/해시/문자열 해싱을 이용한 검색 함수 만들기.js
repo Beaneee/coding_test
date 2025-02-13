61b086a184e8b807ea90043ca62a3f86f264ae30
종빈 <whddkrak@gmail.com>
2025-02-13 23:00:41 +0900

    feat: 문자열 해싱을 이용한 검색 함수 만들기 pseudo code

---
+++ 코딩테스트_합격자되기_자바스크립트/해시/문자열 해싱을 이용한 검색 함수 만들기.js
@@ -0,0 +1,17 @@
+const solution = (stringList, queryList) => {
+  // 문자열 해싱 함수를 생성
+
+  // stringList를 문자열 해싱
+
+  // queryList를 문자열 해싱
+
+  // queryList를 순회하면서 요소가 stringList에 있는지 확인
+
+  // 있다면 true push
+
+  // 없다면 false push
+
+  return [];
+}
+
+console.log(solution(['apple, banana', 'cherry'], ['banana', 'kiwi', 'melon', 'apple']))
