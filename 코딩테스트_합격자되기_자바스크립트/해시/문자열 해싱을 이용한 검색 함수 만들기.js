8336866f779ae207f1d0d7fdc805908e046ab50f
종빈 <whddkrak@gmail.com>
2025-02-19 18:19:54 +0900

    feat: 문자열 해싱을 이용한 검색 함수 만들기

--- 코딩테스트_합격자되기_자바스크립트/해시/문자열 해싱을 이용한 검색 함수 만들기.js
+++ 코딩테스트_합격자되기_자바스크립트/해시/문자열 해싱을 이용한 검색 함수 만들기.js
@@ -1,17 +1,35 @@
-const solution = (stringList, queryList) => {
-  // 문자열 해싱 함수를 생성
+/**
+ * 문자열 해싱
+ * @param str
+ */
+const polynomialHash = (str) => {
+  const p = 31; // 소수
+  const m = 1_000_000_007; // 버킷 크기
+  let hashValue = 0;
 
-  // stringList를 문자열 해싱
-
-  // queryList를 문자열 해싱
-
-  // queryList를 순회하면서 요소가 stringList에 있는지 확인
-
-  // 있다면 true push
-
-  // 없다면 false push
-
-  return [];
+  for (let i = 0; i < str.length; i++) {
+    hashValue = (hashValue * p + str.charCodeAt(i)) % m;
+  }
+  return hashValue
 }
 
-console.log(solution(['apple, banana', 'cherry'], ['banana', 'kiwi', 'melon', 'apple']))
+const solution = (stringList, queryList) => {
+  // stringList를 문자열 해싱
+  const hashList = stringList.map((str) => polynomialHash(str));
+  const result = [];
+
+  for (const query of queryList) {
+    // queryList를 문자열 해싱
+    const queryHash = polynomialHash(query);
+    // queryList를 순회하면서 요소가 stringList에 있는지 확인
+    if(hashList.includes(queryHash)) {
+      result.push(true);
+    } else {
+      result.push(false);
+    }
+  }
+  return result;
+}
+
+console.log(solution(['apple', 'banana', 'cherry'], ['banana', 'kiwi', 'melon', 'apple']))
+console.log(solution(['apple2', 'banana', 'cherry'], ['banana', 'kiwi', 'melon', 'apple']))
