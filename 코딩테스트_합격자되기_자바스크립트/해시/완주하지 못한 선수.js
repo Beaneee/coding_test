64ff68ddd37c1a6cbf21efe99307a926a40c815e
종빈 <whddkrak@gmail.com>
2025-02-25 23:20:00 +0900

    feat: 완주하지 못한 선수 리팩토링

--- 코딩테스트_합격자되기_자바스크립트/해시/완주하지 못한 선수.js
+++ 코딩테스트_합격자되기_자바스크립트/해시/완주하지 못한 선수.js
@@ -1,23 +1,13 @@
 function solution(participant, completion) {
-  const hash = {};
-
   // 참가자 이름으로 hash 값 초기화 및 생성
-  for (const part of participant) {
-    if(hash[part]) {
-      hash[part] = hash[part] + 1;
-    } else {
-      hash[part] = 1;
-    }
-  }
+  const hash = participant.reduce((acc, part) => {
+    acc[part] = (acc[part] || 0) + 1;
+    return acc;
+  }, {});
 
   // 완주한 사람 지워주기
-  for (const comp of completion) {
-    if(hash[comp]) {
-      hash[comp] -= 1;
-    }
-  }
+  completion.forEach(comp => hash[comp]--);
 
   // value가 양수인 사람이 완주하지 못한 사람
-  const positiveKey = Object.entries(hash).find(([key, value]) => value > 0)[0];
-  return positiveKey;
+  return Object.keys(hash).find(key => hash[key] > 0);
 }
