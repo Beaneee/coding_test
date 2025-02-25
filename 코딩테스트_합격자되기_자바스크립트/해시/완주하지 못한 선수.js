d3e236dba4bf38fa065173dd3c2c9838015d4e61
종빈 <whddkrak@gmail.com>
2025-02-25 23:05:39 +0900

    feat: 완주하지 못한 선수 pseudo code

---
+++ 코딩테스트_합격자되기_자바스크립트/해시/완주하지 못한 선수.js
@@ -0,0 +1,11 @@
+function solution(participant, completion) {
+  // 참가자 중에는 동명이인이 있을 수 있습니다.
+
+  // 참가자의 이름으로 hash를 생성한다.
+
+  // 동명이인이 있을 수 있으므로 value는 number로
+
+  // completion에 있으면 hash value - 1;
+
+  // 양수인 hash key를 return 한다.
+}
