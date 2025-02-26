422bc58a36b380b9f532063cbe9f67e46fbe6aed
종빈 <whddkrak@gmail.com>
2025-02-26 18:23:32 +0900

    feat: 할인행사

--- 코딩테스트_합격자되기_자바스크립트/해시/할인행사.js
+++ 코딩테스트_합격자되기_자바스크립트/해시/할인행사.js
@@ -1,9 +1,36 @@
 const solution = (want, number, discount) => {
   // want-number로 hash를 생성
+  const hash = want.reduce((acc, part, idx) => {
+    acc[part] = number[idx];
+    return acc;
+  }, {})
+
+  let initIndx = 0;
+  let startIdx = initIndx;
 
   // discount를 10일까지 순회하면서
+  while (true) {
 
-  // want의 count를 지워줌
+    if(initIndx > discount.length - 1) return 0;
 
-  // 모두 0이 되는 경우 중 가장 짧은 날을 return;
+    const key = discount[startIdx];
+
+    if(hash[key]) {
+      // want의 count를 지워줌
+      hash[key]--;
+    }
+
+    startIdx++;
+
+    // 종료 조건
+    if(startIdx === initIndx + 9) {
+      if(Object.keys(hash).find(key => hash[key] > 0)) {
+        initIndx++;
+        continue;
+      }
+      // 모두 0이 되는 경우 중 가장 짧은 날을 return;
+      return initIndx;
+    }
+
+  }
 }
