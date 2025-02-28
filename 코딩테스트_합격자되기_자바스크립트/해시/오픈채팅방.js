161902e2e3e188ef02eb6296264e95a169923899
종빈 <whddkrak@gmail.com>
2025-02-28 18:36:41 +0900

    feat: 오픈채팅방

--- 코딩테스트_합격자되기_자바스크립트/해시/오픈채팅방.js
+++ 코딩테스트_합격자되기_자바스크립트/해시/오픈채팅방.js
@@ -1,8 +1,38 @@
 function solution(record) {
+  const result = [];
+
   // uuid : nickName hash 생성
   // record를 순회하면 최종적으로 바뀐 nickName이 저장된다.
+  const hash = record.reduce((acc, part) => {
+    const recordSplit = part.split(' ');
+    const key = recordSplit[1];
+    const value =recordSplit[2];
+    acc[key] = value;
+    return acc
+  }, {});
 
-  // Enter, Leave, Change switch 처리
+  for (const rec of record) {
+    const recSplit = rec.split(' ');
+    const uuid = recSplit[1];
+    const action = recSplit[0];
 
-  // return result
+    let message = '';
+    // Enter, Leave, Change switch 처리
+    switch (action) {
+      case "Enter":
+        message = `${hash[uuid]}님이 들어왔습니다.`;
+        break;
+      case "Leave":
+        message = `${hash[uuid]}님이 나갔습니다.`;
+        break;
+      case "Change":
+        break;
+    }
+
+    if(message) {
+      result.push(message);
+    }
+
+  }
+  return result;
 }
