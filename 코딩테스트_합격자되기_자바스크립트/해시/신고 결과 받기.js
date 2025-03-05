5c4d376fd0dbd8cc919a0cd1a8340667cded7d83
종빈 <whddkrak@gmail.com>
2025-03-05 22:58:24 +0900

    feat: 신고 결과 받기

--- 코딩테스트_합격자되기_자바스크립트/해시/신고 결과 받기.js
+++ 코딩테스트_합격자되기_자바스크립트/해시/신고 결과 받기.js
@@ -1,11 +1,39 @@
 function solution(id_list, report, k) {
+  const reportUserHash = {}; // 유저별로 신고 당한 횟수 정보를 담은 hash
 
-  // 유저별로 신고 당한 횟수 정보를 담은 hash
+  // hash 생성
+  for (const rpt of report) {
+    const rptSplit = rpt.split(' ');
+    const reporter = rptSplit[0]; // 신고자
+    const reportedUser = rptSplit[1]; // 신고를 당한 사람
 
-  // 유저가 신고한 유저를 배열로 담은 hash
+    if(!reportUserHash[reportedUser]) {
+      reportUserHash[reportedUser] = new Set();
+    }
 
-  // k번 신고당한 유저 배열
+    reportUserHash[reportedUser].add(reporter);
+  }
 
-  // 유저가 신고한 유저를 배열로 담은 hash에
-  // k번 신고당한 유저가 몇명 있는 지 담은 hash
+  const reportHash = {}; // 정지를 당한 사람을 신고한 횟수를 저장한 hash
+
+  // k번 신고당한 유저
+  for (const [user, count] of Object.entries(reportUserHash)) {
+    if(count.size >= k) {
+      reportUserHash[user].forEach(reporter => {
+        reportHash[reporter] = (reportHash[reporter] || 0) + 1;
+      })
+    }
+  }
+
+  const result = [];
+  // id_list를 순회하면서 reportHash에 담긴 value를 result에 push
+  for (const userId of id_list) {
+    if(!reportHash[userId]) {
+      result.push(0);
+    } else {
+      result.push(reportHash[userId]);
+    }
+  }
+
+  return result;
 }
