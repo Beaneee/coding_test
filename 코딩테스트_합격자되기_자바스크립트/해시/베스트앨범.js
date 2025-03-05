687f3db3a841c94662b3a146e6fdad4848e1d179
종빈 <whddkrak@gmail.com>
2025-03-05 15:55:24 +0900

    feat: 베스트앨범

--- 코딩테스트_합격자되기_자바스크립트/해시/베스트앨범.js
+++ 코딩테스트_합격자되기_자바스크립트/해시/베스트앨범.js
@@ -1,10 +1,32 @@
-function solution(genres, plays) {
+const solution = (genres, plays) => {
+  const result = [];
 
   // genres를 key로 plays를 value로 hash 생성
+  const genreHash = genres.reduce((acc, curr, index) => {
+    acc[curr] = (acc[curr] || 0) + plays[index];
+    return acc
+  }, {});
+
   // 가장 많이 재생된 장르 찾기
+  const sortedGenreKeys = Object.entries(genreHash)
+    .sort((a, b) => b[1] - a[1])
+    .map(([key]) => key);
 
-  // genres-index를 key로 plays hash 생성
-  // 많이 재생된 순으로 노래 찾기
-  // 재생 횟수가 같다면 index가 낮은 것 부터 찾기
+  // 음악 정보를 담은 배열을 생성
+  const playList = genres.reduce((acc, curr, index) => {
+    acc.push({ genre: curr, index, play: plays[index] })
+    return acc;
+  }, [])
 
+  for (const genreKey of sortedGenreKeys) {
+    const list = playList.filter(v => v.genre === genreKey);
+    // 정렬
+    list.sort((a, b) => b.play - a.play);
+
+    const indexList = list.map(v => v.index).filter((_, idx) => idx < 2);
+    // 같은 장르 최대 2개까지만
+    result.push(...indexList);
+  }
+
+  return result;
 }
