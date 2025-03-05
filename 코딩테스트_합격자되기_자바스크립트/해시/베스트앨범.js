998f12ae8bebe7a9ca66c1875e6e313465aa7fe3
종빈 <whddkrak@gmail.com>
2025-03-05 15:09:04 +0900

    feat: 베스트앨범 pseudo code

---
+++ 코딩테스트_합격자되기_자바스크립트/해시/베스트앨범.js
@@ -0,0 +1,10 @@
+function solution(genres, plays) {
+
+  // genres를 key로 plays를 value로 hash 생성
+  // 가장 많이 재생된 장르 찾기
+
+  // genres-index를 key로 plays hash 생성
+  // 많이 재생된 순으로 노래 찾기
+  // 재생 횟수가 같다면 index가 낮은 것 부터 찾기
+
+}
