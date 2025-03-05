22cc063654102c16fe6d820982b8e16d6b81bab8
종빈 <whddkrak@gmail.com>
2025-03-05 14:46:11 +0900

    feat: [level2] 오픈채팅방

---
+++ programmers/level_2/오픈채팅방.js
@@ -0,0 +1,43 @@
+function solution(record) {
+  const result = [];
+
+  // uuid : nickName hash 생성
+  // record를 순회하면 최종적으로 바뀐 nickName이 저장된다.
+  const hash = record.reduce((acc, part) => {
+    const recordSplit = part.split(' ');
+    const action = recordSplit[0];
+
+    if(action !== 'Leave') {
+      const key = recordSplit[1];
+      const value =recordSplit[2];
+      acc[key] = value;
+    }
+
+    return acc
+  }, {});
+
+  for (const rec of record) {
+    const recSplit = rec.split(' ');
+    const uuid = recSplit[1];
+    const action = recSplit[0];
+
+    let message = '';
+    // Enter, Leave, Change switch 처리
+    switch (action) {
+      case "Enter":
+        message = `${hash[uuid]}님이 들어왔습니다.`;
+        break;
+      case "Leave":
+        message = `${hash[uuid]}님이 나갔습니다.`;
+        break;
+      case "Change":
+        break;
+    }
+
+    if(message) {
+      result.push(message);
+    }
+
+  }
+  return result;
+}
