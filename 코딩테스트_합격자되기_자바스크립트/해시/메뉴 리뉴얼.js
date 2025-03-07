61717626cc513d6d098d65b3994d43d676bb24aa
종빈 <whddkrak@gmail.com>
2025-03-07 18:24:31 +0900

    feat: 메뉴 리뉴얼 pseudo code

---
+++ 코딩테스트_합격자되기_자바스크립트/해시/메뉴 리뉴얼.js
@@ -0,0 +1,12 @@
+function solution(orders, course) {
+  const result = [];
+  // course를 순회해서 코스 요리를 구성할 개수로
+
+  // orders에서 생성할 수 있는 조합을 구한다.
+
+  // 조합-count를 hash로 구성한다.
+
+  // 가장 많은 count를 가진 key를 result에 push한다.
+
+  // return result
+}
