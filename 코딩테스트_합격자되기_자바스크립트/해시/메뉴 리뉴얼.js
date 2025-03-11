b95de900b8575cf3c6cac18c1fecd9de2a571b29
종빈 <whddkrak@gmail.com>
2025-03-11 22:58:23 +0900

    feat: 메뉴 리뉴얼

--- 코딩테스트_합격자되기_자바스크립트/해시/메뉴 리뉴얼.js
+++ 코딩테스트_합격자되기_자바스크립트/해시/메뉴 리뉴얼.js
@@ -18,13 +18,11 @@ function getCombinations(arr, selectNum) {
 function solution(orders, course) {
   const result = [];
 
-
   for (const num of course) {
     // 메뉴에서 나올 코스요리를 조합함
     const menuList = [];
     for (const order of orders) {
-      // const orderArr = order.split('').sort();
-      const orderArr = order.split('');
+      const orderArr = order.split('').sort();
       const list = getCombinations(orderArr, num);
       menuList.push(...list);
     }
@@ -33,16 +31,21 @@ function solution(orders, course) {
 
     // 각각 나온 메뉴 구성을 count 한다.
     for (const menu of menuList) {
-      const menuKey = menu.join('')
+      const menuKey = menu.join('');
       courseHash[menuKey] = (courseHash[menuKey] || 0) + 1;
     }
 
-    console.log('courseHash', courseHash)
+    // 가장 많은 count를 가진 key를 result에 push한다.
+    const max = Math.max(...Object.values(courseHash));
+
+    if(max > 1) {
+      for (const [key, value] of Object.entries(courseHash)) {
+        if (value === max) {
+          result.push(key);
+        }
+      }
+    }
   }
 
-
-
-  // 가장 많은 count를 가진 key를 result에 push한다.
-
-  return result
+  return result.sort();
 }
