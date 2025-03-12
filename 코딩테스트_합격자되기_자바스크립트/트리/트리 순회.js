f805aa6b04e8d8ab181cad8213728bd6d91bed4f
종빈 <whddkrak@gmail.com>
2025-03-12 23:38:00 +0900

    feat: 트리 순회

--- 코딩테스트_합격자되기_자바스크립트/트리/트리 순회.js
+++ 코딩테스트_합격자되기_자바스크립트/트리/트리 순회.js
@@ -1,23 +1,50 @@
 // 전위
 const preorder = (nodes, idx) => {
   // 부모 -> 왼쪽 -> 오른쪽
+  if(idx < nodes.length) {
+    let ret = `${nodes[idx]}`;
+    ret += preorder(nodes, idx * 2 + 1);
+    ret += preorder(nodes, idx * 2 + 2);
+    return ret;
+  }
+
+  return "";
 }
 
 // 중위
-const indorder = (nodes, idx) => {
+const inorder = (nodes, idx) => {
   // 왼쪽 -> 부모 -> 오른쪽
+  if(idx < nodes.length) {
+    let ret = inorder(nodes, idx * 2 + 1);
+    ret += `${nodes[idx]}`;
+    ret += inorder(nodes, idx * 2 + 2);
+    return ret;
+  }
+
+  return "";
 }
 
 // 후위
 const postorder = (nodes, idx) => {
   // 왼쪽 -> 오른쪽 -> 부모
+  if(idx < nodes.length) {
+    let ret = postorder(nodes, idx * 2 + 1);
+    ret += postorder(nodes, idx * 2 + 2);
+    ret += `${nodes[idx]}`;
+    return ret;
+  }
+  return "";
 }
 
 
 const solution = (nodes) => {
   // 전위
+  const preOrd = preorder(nodes, 0);
   // 중위
+  const inOrd = inorder(nodes, 0);
   // 후위
+  const postOrd = postorder(nodes, 0);
+  return [preOrd, inOrd, postOrd];
 }
 
 console.log(solution([1,2,3,4,5,6,7]));
