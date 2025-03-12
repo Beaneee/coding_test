73a8060cdf547a504c941ea73e32c8cca2ebaf12
종빈 <whddkrak@gmail.com>
2025-03-12 23:22:37 +0900

    feat: 트리 순회 pseudo code

---
+++ 코딩테스트_합격자되기_자바스크립트/트리/트리 순회.js
@@ -0,0 +1,23 @@
+// 전위
+const preorder = (nodes, idx) => {
+  // 부모 -> 왼쪽 -> 오른쪽
+}
+
+// 중위
+const indorder = (nodes, idx) => {
+  // 왼쪽 -> 부모 -> 오른쪽
+}
+
+// 후위
+const postorder = (nodes, idx) => {
+  // 왼쪽 -> 오른쪽 -> 부모
+}
+
+
+const solution = (nodes) => {
+  // 전위
+  // 중위
+  // 후위
+}
+
+console.log(solution([1,2,3,4,5,6,7]));
