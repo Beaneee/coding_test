8f2fbe4fc3a954635f8919bbf87319e3e367dc47
종빈 <whddkrak@gmail.com>
2025-03-13 18:26:10 +0900

    feat: 이진 탐색 트리 구현 init

---
+++ 코딩테스트_합격자되기_자바스크립트/트리/이진 탐색 트리 구현.js
@@ -0,0 +1,33 @@
+class Node {
+  constructor(key) {
+    this.left = null;
+    this.right = null;
+    this.value = key;
+  }
+}
+
+class BST {
+  constructor() {
+    this.root = null;
+  }
+
+  // 탐색 트리 생성
+  insert(key) {
+
+  }
+
+  // 탐색 트리로 검색
+  search(key) {
+
+  }
+
+}
+
+const solution = (lst, searchList) => {
+  // 탐색 트리 생성
+
+  // 탐색 트리로 검색
+}
+
+console.log(solution([5,3,8,4,2,1,7,10], [1,2,5,6]));
+console.log(solution([1,3,5,7,9], [2,4,6,8,10]));
