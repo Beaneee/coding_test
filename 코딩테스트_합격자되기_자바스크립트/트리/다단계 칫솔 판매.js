0f939c69c477983c30d7e7ff7f50efab8425135f
종빈 <whddkrak@gmail.com>
2025-03-17 16:36:55 +0900

    feat: 다단계 칫솔 판매 pseudo code

---
+++ 코딩테스트_합격자되기_자바스크립트/트리/다단계 칫솔 판매.js
@@ -0,0 +1,10 @@
+function solution(enroll, referral, seller, amount) {
+  // 추천인 트리 또는 해시를 생성한다.
+  console.log(enroll);
+  console.log(referral);
+
+  // seller를 순회하여
+  // 같은 index의 amount * 100으로
+  // parent를 조회하여 10%를 나눈다.
+  // parent가 없을 때 동안 계속 반복한다.
+}
