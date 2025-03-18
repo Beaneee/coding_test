d131c44c869f4d7572fdb6f680f6c860dec122df
종빈 <whddkrak@gmail.com>
2025-03-18 23:24:28 +0900

    feat: dfs pseudo code

---
+++ 코딩테스트_합격자되기_자바스크립트/그래프/DFS.js
@@ -0,0 +1,10 @@
+const solution = (graph, start) => {
+  // 인접 리스트로 만들고
+
+  // dfs 함수를 작성한다.
+
+  // 순회한 값을 return
+}
+
+solution([['A', 'B'], ['B', 'C'], ['C', 'D'], ['D', 'E']], 'A');
+solution([['A', 'B'], ['A', 'C'], ['B', 'D'], ['B', 'E'], ['C', 'F'], ['E', 'F']], 'A');
