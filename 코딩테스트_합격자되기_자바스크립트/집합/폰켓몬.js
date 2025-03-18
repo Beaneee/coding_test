3438dd33c73984c91cc449033b75b1e34bd5f4e2
종빈 <whddkrak@gmail.com>
2025-03-18 17:29:53 +0900

    feat: 폰켓몬

--- 코딩테스트_합격자되기_자바스크립트/집합/폰켓몬.js
+++ 코딩테스트_합격자되기_자바스크립트/집합/폰켓몬.js
@@ -3,8 +3,5 @@ function solution(nums) {
   const setNums = new Set(nums);
   // 선택할 수 있는 수
   const choiceNum = nums.length / 2;
-  // 선택할 수 있는 수보다 setNums.size가 작으면 setNums.size밖에 선택할 수 없다.
-  // 선택할 수 있는 수가 setNums.size보다 크다면 선택할 수 있는 수만큼 종류를 return
-
-  // 저 둘 중 작은 값을 return
+  return Math.min(setNums.size, choiceNum);
 }
