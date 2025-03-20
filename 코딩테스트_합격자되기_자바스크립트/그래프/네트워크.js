8dad25c45a8c33ef958791b94d7e0ff62eeca2b7
종빈 <whddkrak@gmail.com>
2025-03-20 23:56:52 +0900

    feat: 네트워크 pseudo code

---
+++ 코딩테스트_합격자되기_자바스크립트/그래프/네트워크.js
@@ -0,0 +1,19 @@
+const dfs = (graph, node, visited) => {
+  const stack = [];
+  // 시작 노드를 push하고
+  // 방문처리
+
+  // stack이 비어있다면 종료
+
+  // pop
+  // 방문하지 않았다면 방문처리
+
+  // 인접노드를 확인해서
+  // 방문하지 않은 노드들에 대해서만
+  // 재귀
+}
+
+function solution(n, computers) {
+  // 인접 리스트를 만들고
+  // dfs를 진행한다.
+}
