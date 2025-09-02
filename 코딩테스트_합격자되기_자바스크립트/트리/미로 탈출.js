600e1e673ca005678cb2f0c7031bd7b0df049e7f
종빈 <whddkrak@gmail.com>
2025-09-02 22:54:08 +0900

    feat: 미로 탈출

--- 코딩테스트_합격자되기_자바스크립트/트리/미로 탈출.js
+++ 코딩테스트_합격자되기_자바스크립트/트리/미로 탈출.js
@@ -20,19 +20,20 @@ class Queue {
 
 }
 
-// ➎ 큐에서 좌표와 이동 횟수를 꺼냄
-// ➏ 도착점에 도달하면 결과 반환
-// ➐ 네 방향으로 이동
-// ➑ 이동 가능한 좌표인 때에만 큐에 넣음
-// ➋ 방문한 적이 없으면 큐에 넣고 방문 여부 표시
-
 // ➊ 이동 가능한 좌표인지 판단하는 함수
-// ➒ 다음 이동 지점이 물인 경우
-// ➓ 다음 이동 지점이 물이 아닌 경우
+const isValidMove = (ny, nx, n, m, maps) => {
+  return ny >=0 && nx >=0 && ny < n && nx < m && maps[ny][nx] !== 'X'
+}
 
-// ⓫ 도착점에 도달하지 못한 경우
+// ➋ 방문한 적이 없으면 큐에 넣고 방문 여부 표시 함수
+const appendToQueue = (visited, q, ny, nx, k, time) => {
+  if (!visited[ny][nx][k]) {
+    q.push([ny, nx, k, time + 1]); // 시간을 하나 늘림
+    visited[ny][nx][k] = true;
+  }
+}
+
 function solution(maps) {
-  console.log(maps);
   const n = maps.length;
   const m = maps[0].length;
   const visited = Array.from(Array(n), () => Array(m).fill(false).map(() => Array(2).fill(false)));
@@ -62,6 +63,37 @@ function solution(maps) {
     }
   }
 
-  console.log(visited, q)
+  while(!q.isEmpty()) {
+    // ➎ 큐에서 좌표와 레버 스위치 여부 이동 횟수를 꺼냄
+    const [y, x, k, time] = q.pop();
+
+    // ➏ 도착점에 도달하면 결과 반환
+    if(y === endY && x === endX && k === 1) {
+      return time;
+    }
+
+    // ➐ 네 방향으로 이동
+    for (let i = 0; i < 4; i++) {
+      const ny = y + dy[i];
+      const nx = x + dx[i];
+
+      // ➑ 이동 가능한 좌표인 때에만 큐에 넣음
+      if (!isValidMove(ny, nx, n, m, maps)) {
+        continue;
+      }
+
+      // ➒ 다음 이동 지점이 물인 경우
+      if (maps[ny][nx] === 'L') {
+        appendToQueue(visited, q, ny, nx, 1 ,time);
+      } else { // ➓ 다음 이동 지점이 물이 아닌 경우
+        appendToQueue(visited, q, ny, nx, k ,time);
+      }
+
+    }
+
+  }
+
+  // ⓫ 도착점에 도달하지 못한 경우
+  return -1
 }
 
