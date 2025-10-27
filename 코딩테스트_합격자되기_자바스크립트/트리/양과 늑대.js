1a576aa537cd17987fb4a37107b77e752935bac4
종빈 <whddkrak@gmail.com>
2025-10-27 22:19:35 +0900

    feat: 양과 늑대

--- 코딩테스트_합격자되기_자바스크립트/트리/양과 늑대.js
+++ 코딩테스트_합격자되기_자바스크립트/트리/양과 늑대.js
@@ -1,46 +1,49 @@
-const dfs = (graph, node, visited, info, countObj) => {
-  const stack = [];
-  stack.push(node);
+function solution(info, edges) {
+  const n = info.length;
+  const graph = Array.from({ length: n }, () => []);
+  let maxSheep = 0;
 
-  while(stack.length > 0) {
-    const currNode = stack.pop();
-    const target = info[currNode];
 
-    if(!countObj[target]) countObj[target] = 0;
-    countObj[target] += 1;
+  // 1. 트리 구성
+  for (const [p, c] of edges) {
+    graph[p].push(c);
+  }
 
-    if(!visited.has(currNode)) {
-      visited.add(currNode);
+  // 2. dfs
+  const dfs = (sheep, wolf, candidates) => {
+    maxSheep = Math.max(maxSheep, sheep);
+
+    // 후보 노드들 중 하나를 골라 순회
+    for (let i = 0; i < candidates.length; i++) {
+      const next = candidates[i];
+
+      // 방문 시 상태 변화
+      const ns = sheep + (info[next] === 0 ? 1 : 0)
+      const nw = wolf + (info[next] === 1 ? 1 : 0)
+
+      // 양 <= 늑대면 다음 순회
+      if (ns <= nw) continue;
+
+      // 다음 후보 집합 만들기
+      // 이번에 방문한 next는 제거
+      // next의 자식들을 후보에 추가
+      const nextCandidates = [...candidates];
+      nextCandidates.splice(i, 1); // candidates에는 지금 내가 갈 수 있는 모든 노드들의 정보를 가지고있다. 업데이트 해줘야함
+
+      // 예를 들어  [1, 4, 6] 에서 4를 방문하고 난 후 다음에 방문할 수 있는 노드들의 정보는
+      // [1, 6, ...graph[4]] 가 되어야함
+      for (const child of graph[next]) {
+        nextCandidates.push(child);
+      }
+
+      dfs(ns, nw, nextCandidates);
     }
 
-    // 인접한 모든 노드를 확인한다
-    (graph[currNode] || []).forEach(v => {
-      const sheepIdx = 0;
-      const wolfIdx = 1;
-      const isSearch = countObj[sheepIdx] > countObj[wolfIdx];
-
-      if(!visited.has(v) && isSearch) {
-        console.log(countObj);
-        dfs(graph, v, visited, info, countObj);
-      }
-    })
   }
-}
 
-function solution(info, edges) {
-  // 인접 리스트 만들기
-  const graph = {};
-  const countObj = {
-    0: 0,
-    1: 0
-  };
-  const visited = new Set();
+  // 시작: 루트는 바로 방문한다고 가정
+  // 후보는 루트의 자식들
+  dfs(1, 0, [...graph[0]], maxSheep)
 
-  edges.forEach(([u, v]) => {
-    if (!graph[u]) graph[u] = [];
-
-    graph[u].push(v);
-  })
-
-  dfs(graph, 0, visited, info, countObj);
+  return maxSheep;
 }
