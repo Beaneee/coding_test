96629c7c5947f65b7dc057fa4824eb501d394e8d
종빈 <whddkrak@gmail.com>
2025-11-20 22:53:38 +0900

    feat: [level2] 전화번호 목록

--- programmers/level_2/전화번호 목록.js
+++ programmers/level_2/전화번호 목록.js
@@ -1,17 +1,15 @@
 function solution(phone_book) {
-  //비슷한 문자열로 정렬을 하고, 전화번호는 숫자로 되어있으니까~
+  let prefix = '';
   phone_book.sort();
-  // index + 1를 startsWith으로 판별하자., 비슷한 숫자로 정렬하니까,
-  // index + 1를 건너뛴 건 당연히 맞지 않다.
-  for(let i = 0; i < phone_book.length; i++) {
-    const prefix = phone_book[i];
-    const comparePrefix = phone_book[i + 1]
 
-    //더이상 비교할 게 없다는 뜻
-    if(!comparePrefix) return true;
+  for (phone of phone_book) {
+    if (prefix && phone.startsWith(prefix)) {
+      return false
+    }
 
-    if(comparePrefix.startsWith(prefix)) return false
+    prefix = phone;
+
   }
 
-  return true
+  return true;
 }
