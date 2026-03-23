d1cc8df6d8572ab0dbc3cdaa4d6c998d561cf628
종빈 <whddkrak@gmail.com>
2026-03-23 23:11:00 +0900

    feat: 다익스트라 알고리즘

---
+++ 코딩테스트_합격자되기_자바스크립트/그래프/다익스트라 알고리즘.js
@@ -0,0 +1,39 @@
+const solution = (graph, start) => {
+  const dist = {};
+  const visited = new Set();
+  const pq = [[0, start]];
+
+  for (const node of Object.keys(graph)) {
+    dist[node] = Infinity;
+  }
+
+  dist[start] = 0;
+
+  while (pq.length > 0) {
+    pq.sort((a, b) => a[0] - b[0]);
+    const [d, node] = pq.shift();
+
+    // 방문한거에 대해서는 다시 찾지 않는다
+    if (visited.has(node)) continue;
+    visited.add(node);
+
+    for (const [next, weight] of Object.entries(graph[node])) {
+      const updateDist = d + weight;
+
+      if (updateDist < dist[next]) {
+        // 간선 정보 update
+        dist[next] = updateDist;
+        pq.push([updateDist, next]);
+      }
+
+    }
+  }
+
+  console.log(dist);
+}
+
+console.log(solution({ A: { B: 9, C: 3 }, B: { A: 5 }, C: { B: 1 } }, 'A'));
+// [{'A': 0, 'B': 4, 'C': 3}, {'A': ['A'], 'B': ['A', 'C', 'B'], 'C': ['A', 'C']}]
+
+console.log(solution({ A: { B: 1 },B: { C: 5 },C: { D: 1 }, D: {} }, 'A'));
+// [{'A': 0, 'B': 1, 'C': 6, 'D': 7}, {'A': ['A'], 'B': ['A', 'B'], 'C': ['A', 'B', 'C'], 'D': ['A', 'B', 'C', 'D']}]
