1d59bb55c21fcbe85b6c12246e5d437f7edb7aab
종빈 <whddkrak@gmail.com>
2026-03-25 21:56:03 +0900

    feat: 벨만-포드 알고리즘

--- 코딩테스트_합격자되기_자바스크립트/그래프/벨만-포드 알고리즘.js
+++ 코딩테스트_합격자되기_자바스크립트/그래프/벨만-포드 알고리즘.js
@@ -1,11 +1,35 @@
 const solution = (graph, start) => {
   // 간선의 최단 길이 정보를 저장할 변수
+  const n = graph.length;
+  const dist = new Array(n).fill(Infinity);
+  const prev = new Array(n).fill(null);
+  dist[start] = 0;
 
   // n-1번 만큼 반복하면서 계속해서 최단 거리 찾기
+  for (let i = 0; i < n - 1; i++) {
+    for (let u = 0; u < n; u++) {
+      if (dist[u] === Infinity) continue; // 아직 미방문 노드는 스킵
+
+      for (const [v, w] of graph[u]) {
+        if (dist[u] + w < dist[v]) {
+          dist[v] = dist[u] + w;
+          prev[v] = u; // 이전 노드 기록
+        }
+      }
+    }
+  }
 
   // 한번 더 반복해서, 음수 순환이 이루어지는 확인
+  for (let u = 0; u < n; u++) {
 
-  // return
+    for (const [v, w] of graph[u]) {
+      if (dist[u] + w < dist[v]) {
+        return [-1];
+      }
+    }
+  }
+
+  return [dist, prev];
 }
 
 console.log(solution([[[1, 4], [2, 3], [4, -6 ]], [[3, 5]], [[1, 2]], [[0, 7], [2, 4]], [[2, 2]]], 0)) // [[0, -2, -4, 3, -6], [null, 2, 4, 1, 0]]
