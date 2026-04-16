b52256439d55ecfd27e6d50cf6cb577304d2c005
종빈 <whddkrak@gmail.com>
2026-04-16 22:59:42 +0900

    feat: 스도쿠 퍼즐

--- 코딩테스트_합격자되기_자바스크립트/백트래킹/스도쿠 퍼즐.js
+++ 코딩테스트_합격자되기_자바스크립트/백트래킹/스도쿠 퍼즐.js
@@ -1,7 +1,7 @@
 function solution(board) {
   // 현재 위치에 num이 들어갈 수 있는지 체크
   function isValid(num, row, col) {
-    return isRow(num, row) || isCol(num, col) || inBox(num, row, col)
+    return !(isRow(num, row) || isCol(num, col) || inBox(num, row, col))
   }
 
   // 조건1: 해당 행에 넣으려는 숫자 num이 있는지 확인합니다.
@@ -19,12 +19,49 @@ function solution(board) {
 
     for (let i = boxRow; i < boxRow + 3; i++) {
       for (let j = boxCol; j < boxCol + 3; j++) {
-        if (board[boxRow][boxCol] === num) return true
+        if (board[i][j] === num) return true
       }
     }
 
     return false;
   }
+
+  function findEmpty() {
+    for (let i = 0; i < 9; i++) {
+      for (let j = 0; j < 9; j++) {
+        if (board[i][j] === 0) return [i, j]
+      }
+    }
+    return null;
+  }
+
+  function findSolution() {
+    // 빈 칸(0)을 찾는다
+    // 못 찾으면 → 다 채워진 것! return true
+    const emptyPos = findEmpty();
+    if (!emptyPos) return true;
+
+    // 찾았으면 해당 (row, col)에 대해:
+    const [row, col] = emptyPos;
+
+    //   1~9를 하나씩 시도:
+    for (let num = 1  ; num <= 9; num++) {
+      //   isValid 통과하면:
+      if (isValid(num, row, col)) {
+        //   board[row][col] = num  // 놓기
+        board[row][col] = num;
+        // solve() 재귀 호출
+        //         → true 리턴되면 성공, 그대로 return true
+        if (findSolution()) return true;
+        //         → false 리턴되면 board[row][col] = 0  // 되돌리기 (백트래킹)
+        board[row][col] = 0;
+      }
+    }
+    // 시도 다 실패했으면 return false
+    return false;
+  }
+  findSolution();
+  return board;
 }
 
 console.log(
