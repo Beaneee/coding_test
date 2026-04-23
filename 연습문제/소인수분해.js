0a35a881a32be4c556c96a65a5410e38e7c60e5c
종빈 <whddkrak@gmail.com>
2026-04-23 22:58:57 +0900

    feat: [연습문제] 소인수분해

---
+++ 연습문제/소인수분해.js
@@ -0,0 +1,17 @@
+function solution(n){
+  const factors = [];
+
+  for (let i = 2; i * i < n; i++) {
+    while (n % i === 0) {
+      factors.push(i);
+      n /= i;
+    }
+  }
+
+  if (n > 1) factors.push(n);
+  return factors;
+}
+
+console.log(solution(180));
+console.log(solution(2));
+console.log(solution(13));
